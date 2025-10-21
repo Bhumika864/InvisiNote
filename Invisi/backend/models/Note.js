9b894ad4f62d6cf368108bdecf40a25605178d65
Bhumika864 <rajputbhumika121@gmail.com>
2025-10-22 00:35:35 +0530

    feat: Create Note schema for encrypted messages

---
+++ Invisi/backend/models/Note.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema({
+  sender: String,
+  receiver: String,
+  message: String,
+  iv: String,
+  revealDate: Date,
+});
+
+module.exports = mongoose.model("Note", noteSchema);
